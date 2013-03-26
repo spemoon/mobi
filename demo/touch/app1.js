1100ed3bf4b31c293aaf598b58a44eb4f5c49cb4
ld mac <lidian.sw@gmail.com>
2013-03-26 16:21:20 +0800

    调整调用方式

--- demo/touch/app1.js
+++ demo/touch/app1.js
@@ -2,10 +2,10 @@
 
 	$(document).ready(function(){
 		//强制让内容超过     
-		$('#main').css("height",window.innerHeight+100);
+		$('#demo1-wrapper').css("height", window.innerHeight+100);
 		window.scrollTo(0, 1);
 		//重置成新高度     
-		$("#main").css("height",window.innerHeight);
+		$("#demo1-wrapper").css("height", window.innerHeight);
 		//非常重要，用于兼容不同机型，防止浏览器窗口移动     
 		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 		var touchX = new touch({
