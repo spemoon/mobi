61e82882ed80276da0f962aab60b21837ac77ac2
ld mac <lidian.sw@gmail.com>
2013-03-26 15:26:57 +0800

    实验全屏效果

--- demo/touch/app1.js
+++ demo/touch/app1.js
@@ -1,9 +1,16 @@
 (function ($) {
 
 	$(document).ready(function(){
+		//强制让内容超过     
+		$('#main').css("height",window.innerHeight+100);
+		window.scrollTo(0, 1);
+		//重置成新高度     
+		$("#main").css("height",window.innerHeight);
+		//非常重要，用于兼容不同机型，防止浏览器窗口移动     
+		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 		var touchX = new touch({
-			node: 'demo1',
-			debugEnabled: true
+			node: 'demo1'
+			//debugEnabled: true
 		});
 		touchX.init();
 
