04c274dbe2397ce56636b3301e0a8050b551d4f1
caolvchong <caolvchong@gmail.com>
2013-04-28 20:16:38 +0800

    增加qunit网络支持，以便AJAX等测试；增加jshint代码检查

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,30 @@
 module.exports = function(grunt) {
 
+    var server = 'http://localhost/mobi/';
+
     grunt.initConfig({
+        // 代码检查
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                newcap: false,
+                noarg: false,
+                undef: false,
+                sub: false,
+                boss: false,
+                evil: false
+            },
+            uses_defaults: ['js/**/*.js', '!js/lib/util/lang.js', '!js/lib/util/ajax.js'],
+            useEval: { // 允许使用eval或者new Function
+                options: {
+                    evil: true
+                },
+                files: {
+                    src: ['js/lib/util/lang.js', 'js/lib/util/ajax.js']
+                }
+            }
+        },
         // 转化
         transport: {
             options: {
@@ -35,18 +59,25 @@ module.exports = function(grunt) {
         },
         // 单元测试
         qunit: {
-            all: ['test/**/*.html']
+            net: {
+                options: {
+                    urls: [ server + 'test/lib/util/ajax/index.html']
+                }
+            },
+            all: ['test/**/*.html', '!test/lib/util/ajax/index.html']
         },
+        // 文件监听
         watch: {
             files: 'test/**/*.js',
             tasks: ['qunit']
         }
-    })
+    });
 
     grunt.loadNpmTasks('grunt-cmd-transport');
     grunt.loadNpmTasks('grunt-cmd-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
