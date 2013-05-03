4ff3e4439b48ba68492b0e76e630a4fb421bfdde
caolvchong <caolvchong@gmail.com>
2013-05-03 11:30:07 +0800

    jshint配置完善

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,43 @@ module.exports = function(grunt) {
         // 代码检查
         jshint: {
             options: {
-                curly: true,
-                eqeqeq: true,
-                newcap: false,
-                noarg: false,
-                undef: false,
-                sub: false,
-                boss: false,
-                evil: false
+                indent: 4, // 空格缩进数
+                smarttabs: false, // 不允许tab和space混用
+                camelcase: true, // 变量必须使用驼峰或者全部大写以下划线分割，例如 helloWorld, HELLO_WORLD
+                asi: true, // 不能缺失;
+                lastsemic: false, // 不允许缺失最后的;
+                laxcomma: false, // 不允许JSON前置逗号风格
+                debug: true, // 允许使用debugger语句
+                strict: false, // 不需要强制严格模式
+                curly: true, // 循环和条件语句中的语句块必须放在{}
+                eqeqeq: true, // 相等比较需要用 === 或 !==
+                eqnull: true, // null允许用==判断相等
+                newcap: false, // 构造函数必须大写，这个不要求
+                noarg: false, // 允许使用argument.callee 和 argument.caller
+                nonew: false, // 允许不做赋值的构造函数
+                noempty: false, // 允许使用空函数
+                plusplus: false, // 允许使用++和--
+                latedef: true, // 局部变量必须先声明再使用
+                undef: true, // 禁止使用未声明的变量
+                browser: true, // 浏览器环境，支持除了alert/console外的全局变量
+                globals: { // 排除的全局变量
+                    seajs: true,
+                    define: true,
+                    module: true
+                },
+                trailing: true, // 不允许行尾有空白
+                devel: false, // 不开启alert/console全局变量支持
+                unused: false, // 允许变量声明后不使用，常见于一些函数调用上的参数没有使用
+                quotmark: 'single', // 字符串引号必须使用单引号
+                sub: false, // 允许使用[]来访问对象属性
+                boss: false, // 允许if/while/for中使用赋值语句
+                immed: false, // 允许使用匿名函数并立即执行
+                forin: false, // 允许for in中不强制hasOwnProperty检测
+                bitwise: false, // 允许位运算
+                es5: true, // 允许使用ES5特性
+                evil: false // 不允许使用eval和new Function
             },
-            uses_defaults: ['js/**/*.js', '!js/lib/util/lang.js', '!js/lib/util/ajax.js'],
+            useDefault: ['Gruntfile.js', 'js/**/*.js', '!js/lib/util/lang.js', '!js/lib/util/ajax.js'],
             useEval: { // 允许使用eval或者new Function
                 options: {
                     evil: true
