8d02825a3e6edc27db915fee8142b6f893fcef4e
caolvchong <caolvchong@gmail.com>
2013-05-08 17:35:00 +0800

    seajs对于本地与线上开发模式的配置

--- public/js/config.js
+++ public/js/config.js
@@ -1,15 +1,23 @@
-define(function(require, exports, module) {
+(function() {
     var development = true;
+    var version = '0.0.1';
+
     var plugins = ['shim'];
     var map = [];
-    if(development) {
+    if(development) { // 开发模式
         plugins.push('nocache');
+        var dist = 'public/js/dist/';
+        var src = 'js/'
+
         map.push(function(url) {
-            var dist = 'public/js/dist/';
-            var src = 'js/src/app/'
             if (url.indexOf(dist) > 0) {
                 url = url.replace(dist, src);
             }
+            return url;
+        });
+    } else { // 本地部署模式/线上模式
+        map.push(function(url) {
+            url += (url.indexOf('?') === -1 ? '?' : '&') + '_v=' + version;
             return url;
         });
     }
@@ -18,10 +26,10 @@ define(function(require, exports, module) {
         plugins: plugins,
         map: map,
         alias: {
-            'jquery': {
+            jquery: {
                 src: 'jquery/jquery-1.9.1.min.js',
                 exports: 'jQuery'
             }
         }
     });
-});
+})();
